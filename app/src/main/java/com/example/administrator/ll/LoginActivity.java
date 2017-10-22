42f1d442b85cc6335d37804b5a32248ebf424041
pear123 <997672613@qq.com>
2017-10-22 11:39:01 +0800

    20171022

--- app/src/main/java/com/example/administrator/ll/LoginActivity.java
+++ app/src/main/java/com/example/administrator/ll/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.administrator.ll;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by Administrator on 2017/10/16.
@@ -14,5 +17,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView jump=(TextView) findViewById(R.id.Login_TextView_Send);
+        jump.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent =new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
