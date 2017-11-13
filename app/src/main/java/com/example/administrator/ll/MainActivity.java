96abc82c013f99dfe42ef59a686af780eeaedf1f
pear123 <997672613@qq.com>
2017-11-13 11:50:39 +0800

    11/10

--- app/src/main/java/com/example/administrator/ll/MainActivity.java
+++ app/src/main/java/com/example/administrator/ll/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.administrator.ll;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +16,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+               setSupportActionBar(toolbar);
+
+
+
+
+        ImageView jump=(ImageView) findViewById(R.id.imageButton3);
+        jump.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this, MyActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
+
+
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
